a2630e1b9ec521b283228194671bd7cfcb10c512
soroush <soroushjorjorzade@gmail.com>
2022-01-24 18:09:01 +0330

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Ferox on 1/24/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
